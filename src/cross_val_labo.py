077a62c841e569a8f2536914056d08395c2fcfe8
Dan Ringwald <dan.ringwald12@gmail.com>
2019-09-11 18:11:08 +0200

    got 89% with all features, trained on train split, tested on val split, not greedy params

--- src/cross_val_labo.py
+++ src/cross_val_labo.py
@@ -6,7 +6,7 @@ import lightgbm as lgb
 
 class CrossValLabo(object):
     @staticmethod
-    def optimize_hparams(model, param_set, X_train, Y_train, X_val, Y_val, save=False):
+    def optimize_hparams(model, params, param_set, X_train, Y_train, X_val, Y_val, save=False):
         clf = GridSearchCV(model, param_set, n_jobs=1, 
                     cv=[(
                             np.arange(0, Y_train.shape[0], 1),
@@ -16,39 +16,12 @@ class CrossValLabo(object):
         X = pd.concat((X_train, X_val))
         Y = pd.concat((Y_train, Y_val))
         clf.fit(X, Y)
-        best_parameters_val, score_val, model = clf.best_params_, clf.best_score_, clf.best_estimator_
+        best_parameters_val = clf.best_params_
+        params = {**params, **best_parameters_val}
+        model = lgb.LGBMClassifier(**params)
+        model.fit(X_train, Y_train)
         preds_train = model.predict_proba(X_train)[:, 1]
         score_train = roc_auc_score(Y_train, preds_train)
+        preds_val = model.predict_proba(X_val)[:, 1]
+        score_val = roc_auc_score(Y_val, preds_val)
         return best_parameters_val, score_val, score_train, model
-        
-    @staticmethod
-    def quick_lgbm(param_set, X_train, Y_train, X_val, Y_val):
-        tr_data = lgb.Dataset(X_train, label=Y_train)
-        vl_data = lgb.Dataset(X_val, label=Y_val)  
-
-        mdl = lgb.LGBMRegressor(
-            boosting_type='gbdt',
-            objective='regression',
-            metric='auc',
-            learning_rate=0.01,
-            num_leaves=2**4,
-            max_depth=5,
-            tree_learner='serial',
-            colsample_bytree=0.80,
-            subsample_freq=1,
-            subsample=1,
-            n_estimators=2**10,
-            max_bin=255,
-            verbose=-1,
-            seed=1337,
-            early_stopping_rounds=100,
-            reg_alpha=0.3,
-            reg_lamdba=0.243,
-            n_jobs = -1)
-
-        estimator = lgb.train(
-            param_set,
-            tr_data,
-            valid_sets = [tr_data, vl_data],
-            verbose_eval = 200,
-        )
